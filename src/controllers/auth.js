6804adc1cad5788021d2d317e893a43fecec3760
Carolina Fernandez <carolinafernandezbeltran@gmail.com>
2020-10-26 19:19:48 -0300

    seteado al puerto 9000

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,11 +1,11 @@
-const { jwtMock } = require('../mocks');
+const { jwtMock } = require("../mocks");
 const auth = (req, res) => {
-    /**
-     * call DB lookup for user
-     *  if succeed -> 200 and redirect page
-     *  if failure -> 403 FORBIDDEN
-     */
-    // res.status(403).end();
-    res.json({ redirect: '/home', jwt: jwtMock });
+  /**
+   * call DB lookup for user
+   *  if succeed -> 200 and redirect page
+   *  if failure -> 403 FORBIDDEN
+   */
+  // res.status(403).end();
+  res.json({ redirect: "/patients", jwt: jwtMock });
 };
 module.exports = auth;
