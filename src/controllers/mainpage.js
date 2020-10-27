70379c86c5abaeb81a6f3cefdf15a8885cd92365
Luciano Pérez Cerra <luchopcerra@gmail.com>
2020-10-26 21:53:48 -0300

    agregué la primera query

--- src/controllers/mainpage.js
+++ src/controllers/mainpage.js
@@ -2,11 +2,26 @@ const { mainpageDoctorMock } = require('../mocks');
 const getDoctorData = (id) => {
     return mainpageDoctorMock;
 };
-const mainPage = (req, res) => {
+const dbAPI = require('../database/database');
+
+const queries = require('../database/queries');
+const mainPage = async (req, res) => {
     /**
      * read JWT to find user kind
      */
-    const data = getDoctorData();
-    res.json(data);
+    const mockedData = getDoctorData();
+    const trx = await dbAPI.start();
+    const queryResult = await queries.findUserByEmail("javier@gmail.com", trx);
+    await dbAPI.commit(trx);
+    res.json({
+        ...mockedData, ...{
+            user: {
+                role: queryResult.role,
+                name: queryResult.name,
+                lastname: queryResult.lastname,
+                system: queryResult.system_name
+            }
+        }
+    });
 };
 module.exports = mainPage;
